e193b3882305e59520fecef471916544f2cada69
Alexandre Amory <amamory@gmail.com>
2020-12-05 11:11:27 -0300

    improving debug msgs for the sorted knapsack

--- 01_knapsack/solutions/sorted_value_per_weight.py
+++ 01_knapsack/solutions/sorted_value_per_weight.py
@@ -4,6 +4,42 @@
 from collections import namedtuple
 Item = namedtuple("Item", ['index', 'value', 'weight'])
 
+# assign False to submit the solution
+debug = True
+
+def linear_relaxation(items, capacity):
+    room = capacity
+    item = 0
+    estimate = 0.0
+    while room > 0 and item < len(items):
+        if room-items[item].weight >= 0:
+            room -= items[item].weight
+            estimate += items[item].value
+        else:
+            room = float(room) / float(items[item].weight)
+            estimate += float(room) * items[item].value
+            break
+        item +=1
+    return estimate
+
+def print_table(items, taken):
+    sum_value = 0
+    sum_weight = 0
+    print("_______________________________")
+    print(' {:10s} {:10s} {:10s} '.format("Index","Value","Weight"))
+    print("_______________________________")
+    idx = 0
+    for i in items:
+        if taken[idx] == 1:
+            print(' {:5d} {:10d} {:10d} '.format(i.index, i.value, i.weight))
+            sum_value += i.value
+            sum_weight += i.weight
+        idx +=1
+    print("_______________________________")
+    print(' {:16d} {:10d} '.format(sum_value, sum_weight))
+    print ("")
+
+
 def solve_it(input_data):
     """ sort items in reverse order of value/weight ratio.
 
@@ -29,15 +65,36 @@ def solve_it(input_data):
     weight = 0
     taken = [0]*len(items)
 
+    if debug:
+        print ("")
+        print ("Capacity: %d, #items: %d" % (capacity, item_count))
+        print ("Input:")
+        print_table(items, [1]*item_count)
+
+
     # items sorted in reverse order of value/weight ratio
     items = sorted(items, key=lambda x: float(x.value/float(x.weight)))[::-1]
 
+    taken_debug = [0]*item_count
+    idx = 0
     for item in items:
         if weight + item.weight <= capacity:
             taken[item.index] = 1
+            taken_debug[idx] = 1
             value += item.value
             weight += item.weight
+        idx +=1
     
+    if debug:
+        print ("")
+        estimate = linear_relaxation(items,capacity)
+        print ("Estimated value: %.2f" % (estimate))
+        print (", ".join(str(i) for i in items))
+        print ("")
+        print ("Solution:")
+        print_table(items, taken_debug)
+
+
     # prepare the solution in the specified output format
     output_data = str(value) + ' ' + str(0) + '\n'
     output_data += ' '.join(map(str, taken))
