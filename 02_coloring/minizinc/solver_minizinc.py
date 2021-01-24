9075593b51b2b093fc21dad87456f398c5043066
Alexandre Amory <amamory@gmail.com>
2021-01-24 14:51:52 -0300

    incomplete loop for graph coloring

--- 02_coloring/minizinc/solver_minizinc.py
+++ 02_coloring/minizinc/solver_minizinc.py
@@ -83,13 +83,16 @@
 # https://github.com/discreteoptimization/setcover/blob/master/minizinc_001/solver.py
 
 import os
-from threading import Timer
 from subprocess import Popen, check_output, PIPE, TimeoutExpired
 import networkx as nx
 from networkx.algorithms.approximation import clique
+import matplotlib.pyplot as plt 
 import statistics 
 import math
+import random
+import time
 
+DEBUG = True
 
 def solve_it(input_data):
     # parse the input
@@ -107,74 +110,124 @@ def solve_it(input_data):
     # generate NetworkX graph
     G = nxGraph(edges)
 
-    # kill = lambda process: process.kill()
-    # cmd = ['ping', 'www.google.com']
-    # ping = subprocess.Popen(
-    #     cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # my_timer = Timer(5, kill, [ping])
-    # try:
-    #     my_timer.start()
-    #     stdout, stderr = ping.communicate()
-    # finally:
-    #     my_timer.cancel()
+    # set variables for process communication with minizinc
+    process = None
+    stdout = None
+    stderr = None
+    global_exitcode = None
 
 
     # find the max_clique as a lower bound. at least this number of colors are required
-    # not recommended for graphs with more than 500 nodes
-    max_clique = clique.max_clique(G)
-    print ('max_clique:', len(max_clique), max_clique)
-    # according to , sec 2.2.2 Upper bounds
-    # the max degree can be used as an upper bound
+    max_clique = [1]
+    if node_count <= 500:
+        # not recommended for graphs with more than 500 nodes
+        max_clique = clique.max_clique(G)
+    if DEBUG:
+        print ('max_clique:', len(max_clique), max_clique)
+        
+    # According to the book: "A Guide to Graph Colouring: Algorithms and Applications", 
+    #  Section '2.2.2 Upper bounds',  the max degree can be used as an upper bound
     degrees = [0]*node_count
     for node in range(node_count):
         degrees.append(G.degree(node)+1)
     #remove the zeros from the list
     while 0 in degrees: degrees.remove(0)
     max_degree = max(degrees)
-    max_degree = int(math.ceil(statistics.median(degrees)))
-    print ('max_degree:', max_degree, degrees)
-
-    # generate MiniZinc data file
-    data_file = "data.dzn"
-    generateMinizincDataFile(node_count, edge_count, len(max_clique), max_degree, edges, data_file)
-
-    # solve with Minizinc's MIP solver (CBC of COIN-OR)
+    med_degree = int(math.ceil(statistics.median(degrees)))
+    if DEBUG:
+        print ('max_degree:', max_degree, degrees)
+        print ('med_degree:', med_degree)
+    lb = len(max_clique)
+    # the 1st atempt is to use the median as upper bound instead of the true upper bound max_degree
+    initial_step = 3
+    ub = min(lb+initial_step, med_degree)
+    step = initial_step
     timeout = 10
+    while_abort = False
+    solution_found = False
+    satisfied = False
     aborted = False
-    process = None
-    stdout = None
-    stderr = None
-    global_exitcode = None
-    minizinc_proc = Popen(['minizinc', '-m', 'graphColoring.mzn', '-d', 'data.dzn'],
-                   stdout=PIPE, stderr=PIPE)
-    try:
-        #x=subprocess.check_output(command, shell=True, timeout=5)
-        (stdout, stderr) = minizinc_proc.communicate(timeout=timeout)
-    except TimeoutExpired as exc:
-        minizinc_proc.kill()
-        aborted = True
-    #else:
-    #    print (process)    
-    #process = Popen(['minizinc', '-m', 'graphColoring.mzn', '-d', 'data.dzn'],
-    #                stdout=PIPE, stderr=PIPE)
-    # ALTERNATIVE_1: solve with Minizinc's CP solver
-    # process = Popen(['mzn-g12fd', '-n', str(nb_solutions),'setCovering.mzn', 'data.dzn'],
-    #                stdout=PIPE, stderr=PIPE)
-    # ALTERNATIVE_2: solve with Minizinc's CP solver that uses learning via lazy clause generation
-    # process = Popen(['mzn-g12lazy', '-n', str(nb_solutions),'setCovering.mzn', 'data.dzn'],
-    #                stdout=PIPE, stderr=PIPE)
-    # ALTERNATIVE_3: solve with CP solver Gecode (however, Gecode must be installed to do that!)
-    # process = Popen(['mzn-gecode', '-n', str(nb_solutions),'setCovering.mzn', 'data.dzn'],
-    #                stdout=PIPE, stderr=PIPE)
+    while not while_abort:
+        if aborted:
+            # if the current execution is too long, then reduce the ub and the step 
+            # until reaching an acceptable execution time of find out that it's not possible 
+            # to run the model with the current timeout
+            if step > 1 and satisfied:
+                step -= 1
+                ub = lb + step
+            else:
+                # if lb + 2*initial_step < max_degree:
+                #     ub = lb + 2*initial_step+1
+                #     lb = lb + initial_step+1
+                # elif lb < max_degree:
+                #     ub = max_degree
+                #     lb = lb + initial_step+1
+                # else:
+                if lb > max_degree:
+                    # abort because all reached the true upper bound. nothing else to search
+                    print ("upper bound reached and no solution was found")
+                    while_abort = True
+                    break
+                ub = min(lb + 2*initial_step+1, max_degree)
+                lb = lb + initial_step+1
+        else:
+            print("NOT ABOOOORTED!")
 
-    #(stdout, stderr) = minizinc_proc.communicate()
+        if DEBUG:
+            print ("running with lb:", lb, "and ub:", ub, 'step:', step)
+        # generate MiniZinc data file
+        data_file = "data.dzn"
+        generateMinizincDataFile(node_count, edge_count, lb, ub, edges, data_file)
 
+        # solve with Minizinc's MIP solver (CBC of COIN-OR)
+        aborted = False
+        minizinc_proc = Popen(['minizinc', '-m', 'graphColoring.mzn', '-d', 'data.dzn'],
+                stdout=PIPE, stderr=PIPE)
+        try:
+            (stdout, stderr) = minizinc_proc.communicate(timeout=timeout)
+        except TimeoutExpired as exc:
+            minizinc_proc.kill()
+            time.sleep(1)
+            aborted = True
+            if DEBUG:
+                print ('Timeout!')
+        # any other exception
+        except Exception as e:
+            minizinc_proc.kill()
+            time.sleep(1)
+            aborted = True
+            if DEBUG:
+                print ('Unknown error!')
+        else:
+            aborted = False
+            # solution found, can abort the while loop
+            if DEBUG:
+                print ("solution found")
+                print (stdout)
+                print (stderr)
+            satisfied = str(stdout, 'utf-8')
+            lines = satisfied.split('\n')
+            # continue the search if it is not satisfied with these bounds
+            if 'UNSATISFIABLE' not in lines[0]:
+                while_abort = True
+                solution_found = True
+                satisfied = False
+                break
+            else:
+                satisfied = True
+
+        # if not aborted:
+        #     lb += step
     # print error messages if there are any 
     #print (stderr)
+
     output_data = None
-    if not aborted:
+    if solution_found:
         # extract the solution from standard-out
         colors, solution = extractSolution(stdout,node_count)
+
+        # generate the colored graph
+        graph_dot(G, int(colors), solution)
 
         # prepare the solution in the specified output format
         output_data = str(colors) + ' ' + str(1) + '\n'
@@ -184,7 +237,7 @@ def solve_it(input_data):
 
     return output_data
 
-# ##################################################################################
+###################################################################################
 def nxGraph(edges):
     G = nx.Graph()
 
@@ -193,8 +246,73 @@ def nxGraph(edges):
     
     return G
 
+###################################################################################
+def graph_dot(G, colors, solution):
 
-# ##################################################################################
+    if DEBUG:
+        print ('writing the graph ...')
+    # open the color file
+    color_file = open('svg_colors', 'r')
+    color_data = ''.join(color_file.readlines())
+    color_file.close()
+    lines = color_data.split('\n')
+    # remove the trailing white spaces
+    lines = [ele.replace(' ', '') for ele in lines]
+    lines = [ele.replace('\t', '') for ele in lines]
+
+    selected_colors = []
+    # randomly select a set of colors
+    i = colors
+    while i>0:
+        new_color = lines[random.randint(0, len(lines)-1)]
+        if new_color not in selected_colors:
+            selected_colors.append(new_color)
+            i -=1
+
+    if DEBUG:
+        print ('selected colors:', selected_colors)
+        print ('solution:', solution)
+
+    # transforms NetworkX into PyG AGraph
+    A = nx.nx_agraph.to_agraph(G)
+    #print (A.to_string())
+
+    for i in range(len(solution)):
+        n=A.get_node(i)
+        color_code = solution[i]-1
+        #print ('2:',n, solution[n], selected_colors[color_code] )
+        n.attr['fillcolor'] = selected_colors[color_code]
+        n.attr['style']='filled'
+
+    # create the legend
+    legend_nodes = []
+    for i in range(len(selected_colors)):
+        node_name = 'c'+str(i)
+        A.add_node(node_name)
+        n=A.get_node(node_name)
+        n.attr['fillcolor'] = selected_colors[i]
+        legend_nodes.append(node_name)
+        n.attr['style']='filled'
+    A.add_subgraph(name='cluster1', nbunch= legend_nodes, label='legend', color = 'black', rank='same')
+
+    A.write("graph.dot")
+    A.layout(prog='dot')
+    A.draw("graph.png")
+
+###################################################################################
+def graph_pyplot(G, colors, solution):
+
+    for n in G.nodes():
+        #print ('1:',n, solution[n] )
+        color_code = solution[n]-1
+        #print ('2:',n, solution[n], selected_colors[color_code] )
+        G.nodes[n]['color'] = selected_colors[color_code]
+        #print ('3:',n, solution[n], selected_colors[color_code] )
+
+    nx.draw(G, with_labels = True) 
+    plt.savefig("graph.svg") 
+
+###################################################################################
 def generateMinizincDataFile(node_count, edge_count, lb, ub, edges, data_file):
     tmpFile = open(data_file, 'w')
     
@@ -227,8 +345,6 @@ def generateMinizincDataFile(node_count, edge_count, lb, ub, edges, data_file):
 # ##################################################################################
 def extractSolution(stdout,node_count):
     solution = [0]*node_count
-    #for i in range(0, node_count):
-    #    solution.append(0)
     
     stdout = str(stdout, 'utf-8')
     lines = stdout.split('\n')   
